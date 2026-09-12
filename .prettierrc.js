e253203be1c30090f4bdf51b379251ed31426df1
Tengfei Han <30319402+Freedom9888@users.noreply.github.com>
2026-09-12 11:42:27 +0800

    chore: add prettier config, editorconfig, and update gitignore

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+# Prettier 配置
+# 详见: https://prettier.io/docs/en/options.html
+
+module.exports = {
+  # 单引号
+  singleQuote: true,
+  # 制表符宽度
+  tabWidth: 2,
+  # 不使用分号
+  semi: false,
+  # 尾部逗号
+  trailingComma: 'es5',
+  # 箭头函数单个参数不使用括号
+  arrowParens: 'avoid',
+  # 单行最大字符数
+  printWidth: 100,
+}
